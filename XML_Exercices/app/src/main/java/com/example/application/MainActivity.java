65f73c824b74cd0760f7d57070189d7f631ee0a5
Adil <adil@gmail.com>
2022-10-29 13:06:01 +0100

    xml_exercice

---
+++ XML_Exercices/app/src/main/java/com/example/application/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ecommerceapp);
+    }
+
+}
